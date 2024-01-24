fbbe85f2621a831402e0a0f59fcf778b9d9180d3
Ivan Bacigaluppo <ivanbac1896@gmail.com>
2024-01-24 19:45:02 -0300

    feat: :sparkles: Make request to API and print houses information

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest;
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct House {
+    id: u128,
+    address: String,
+    homeowner: String,
+    price: u128,
+    photoURL: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct ApiResponse {
+    houses: Vec<House>,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let url = "http://app-homevision-staging.herokuapp.com/api_project/houses";
+
+    let response = reqwest::get(url).await?;
+
+    let data = response.json::<ApiResponse>().await?;
+
+    println!("{:#?}", data.houses);
+
+    Ok(())
 }
